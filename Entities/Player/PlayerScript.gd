5d6acda6a27e4ce0055e94c9847d3cea0dee5a46
Albert Valo <albert@aluniosono.net>
2020-09-01 22:55:55 +0200

    Multiple fixes for Ava
    Ava animation fixed, double hit fixed, animation tree fixed

--- Entities/Player/PlayerScript.gd
+++ Entities/Player/PlayerScript.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 # JUMP HEIGHT
 const JUMP_H = -900
 const UP = Vector2(0, -1)
-var GRAVITY = 40.0
 
+var gravity = 40.0
 var acceleration
 var max_speed
 var sprite
@@ -17,14 +17,14 @@ var velocity
 var base_acceleration
 var base_max_speed
 var jumping
-
 var is_dead : bool
-export var ready_to_jump: bool
+
+export var attacking : bool
 
 func _ready():
 	# IN CASE YOU NEED TO MODIFY ANY VALUE, HERE IS THE PLACE
-	base_acceleration = 90
-	base_max_speed = 500
+	base_acceleration = 100
+	base_max_speed = 550
 	
 	acceleration = base_acceleration
 	max_speed = base_max_speed
@@ -32,94 +32,112 @@ func _ready():
 	# COUNTER FOR THE DOUBLE JUMP
 	jumpCounter = 0
 	dashCounter = 0
+
+	sprite = $Sprite
+	stateMachine = $AnimationTree.get("parameters/playback")
+	stateMachine.start("Idle")
 	velocity = Vector2.ZERO
 	
-	sprite = $Sprite
-	#animationPlayer = $AnimationPlayer
-	stateMachine = $AnimationTree.get("parameters/playback")
-	
-	stateMachine.start("Idle")
 	jumping = false
-	ready_to_jump = true
 	is_dead = false
+	attacking = false
 	pass
 	
-func _process(delta):
+func _process(_delta):
 	
-	velocity.y += GRAVITY
-	get_input(friction)
+	# DEBUG OPTIONS
+	
+	if Input.is_action_pressed("is_dead"):
+		stateMachine.travel("die")
+		print("You killed the player")
+		is_dead = true
+	pass
+	
+	print(stateMachine.get_current_node())
+	print("attacking: ", attacking)
+	
+	### END DEBUG OPTIONS
+	
+	
+	velocity.y += gravity
+	get_input()
 	dash()
 	velocity = move_and_slide(velocity, UP)
-
+	if is_dead:
+		set_process(false)
 	pass
 
 # FUNCTION TO GET PLAYER CONTROL
-func get_input(friction):
-	if Input.is_action_pressed("ui_right"):
-		if !jumping:
-			stateMachine.travel("Run")
-		sprite.flip_h = false
-		velocity.x = min(velocity.x + acceleration, max_speed)
-	elif Input.is_action_pressed("ui_left"):
-		if !jumping:
-			stateMachine.travel("Run")
-		sprite.flip_h = true
-		velocity.x = max(velocity.x - acceleration, -max_speed)
-	else:
-		if !jumping:
-			stateMachine.travel("Idle")
-		friction = true
-	
-	if is_on_floor():
-		jumping = false
-		ready_to_jump = true
+func get_input():
+	# MOVEMENT
+	stateMachine.travel("Idle")
 		
+	if attacking == false:
+		if Input.is_action_pressed("ui_right"):
+			stateMachine.travel("Run")
+			sprite.scale.x = 1
+			velocity.x = max_speed
+		elif Input.is_action_pressed("ui_left"):
+			stateMachine.travel("Run")
+			sprite.scale.x = -1
+			velocity.x = -max_speed
+		
+		# DASH INPUT
+		if Input.is_action_just_pressed("dash"):
+			stateMachine.travel("Run")
+			dash()
+	pass
+
+	if is_on_floor():
+		print("is on floor")
+		jumping = false
+		velocity.x = lerp(velocity.x, 0, 0.2)
 		jumpCounter = 0
 		dashCounter = 0
+		
 		if Input.is_action_just_pressed("action"):
-			stateMachine.travel("Attack")
-			print("attack")
-			pass
+			if stateMachine.get_current_node() == "Attack_1":
+				stateMachine.travel("Attack_2")
+				print("attack 1")
+			else:
+				stateMachine.travel("Attack_1")
+				print("attack 2")
+		pass
 		
-		if friction == true:
-			velocity.x = lerp(velocity.x, 0, 0.5)
-		
-		if Input.is_action_just_pressed("ui_accept"):
-			jumping = true
-			ready_to_jump = false
-			
-			stateMachine.travel("Jump")
-			jumpCounter += 1
-			velocity.y = JUMP_H
-		if friction == true:
-			velocity.x = lerp(velocity.x, 0, 0.01)
+		if !attacking:
+			if Input.is_action_just_pressed("ui_accept"):
+				jumping = true
+				
+				stateMachine.travel("Fall 2")
+				jumpCounter += 1
+				velocity.y = JUMP_H
 	else:
 		print("is not on floor")
-		
-		if jumping == false:
-			stateMachine.travel("Fall 2")
+		velocity.x = lerp(velocity.x, 0, 0.03)
 
+		if jumping == false:
+			stateMachine.travel("Fall")
 		else:
-			if jumpCounter < 2 and ready_to_jump and Input.is_action_just_pressed("ui_accept"):
-				ready_to_jump = false
+			stateMachine.travel("Fall 2")
+			if jumpCounter < 2 and Input.is_action_just_pressed("ui_accept"):
 				stateMachine.travel("Jump")
 				jumpCounter += 1 
-				velocity.y = JUMP_H	
+				velocity.y = JUMP_H
 	pass
 
 # DASH FUNCTION, MODIFIES ORIGINAL PLAYER MAX SPEED AND ACCELERATION
 func dash():
-	if  Input.is_action_just_pressed("dash") && dashCounter < 1:
+	if  Input.is_action_just_pressed("dash") and dashCounter < 1:
 		dashCounter += 1
 		max_speed = 2000
 		acceleration = 1000
-		GRAVITY = 0
+		gravity = 0
 		$Timer.start()
 	pass
 
 # TIME OUT FOR THE DASH AND GET MAX SPEED AND ACCELERATION BACK TO ORIGINAL VALUES
 func _on_Timer_timeout():
 	max_speed = base_max_speed
-	GRAVITY = 40.0
+	gravity = 40.0
 	acceleration = base_acceleration
 	pass
