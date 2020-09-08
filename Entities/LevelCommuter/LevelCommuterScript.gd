c9aaad7c8eee3bdf2d929beb3f63174899a2b376
Albert Valo <albert@aluniosono.net>
2020-09-08 20:24:30 +0200

    Navigation fixed without effect

--- Entities/LevelCommuter/LevelCommuterScript.gd
+++ Entities/LevelCommuter/LevelCommuterScript.gd
@@ -1,22 +1,23 @@
 extends Area2D
 
-export (PackedScene) var next_level
+export (String) var next_level_name
 export (String) var player_position
 var player_health : int
 
+var next_level
+
+func _ready():
+
+	next_level = "res://Scenes/DebugLevels/"+next_level_name+".tscn"
 func _physics_process(_delta):
 	var bodies = get_overlapping_bodies()
 	
 	for body in bodies:
 		if body.name == "Player":
 			load_level(player_position, body.health)
-			
-			
-			print("player colliding")
 	pass
 
 func load_level(player_position, player_health):
 	SceneSwitcher.setPlayerHealth(player_health)
 	SceneSwitcher.setPlayerPosition(player_position)
-	
-	get_tree().change_scene_to(next_level)
+	get_tree().change_scene(next_level)
